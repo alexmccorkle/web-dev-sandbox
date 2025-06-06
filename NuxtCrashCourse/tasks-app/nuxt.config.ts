cf1e63dfa5f3ba168819cf3ff994e3a327fe1fce
alexmccorkle <114952775+alexmccorkle@users.noreply.github.com>
2025-06-06 15:37:36 +0200

    init commit for nuxt app

---
+++ NuxtCrashCourse/tasks-app/nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+	modules: ['@nuxt/eslint'],
+	devtools: { enabled: true },
+	compatibilityDate: '2025-05-15',
+	eslint: {
+		config: {
+			stylistic: {
+				indent: 'tab',
+				quotes: 'single',
+				semi: true,
+				commaDangle: 'always-multiline',
+			},
+		},
+	},
+});
