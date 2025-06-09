83db0671435097ed2b0173a2dc4367adf6e5963a
alexmccorkle <114952775+alexmccorkle@users.noreply.github.com>
2025-06-09 14:34:44 +0200

    added landing page w navbar and light/dark toggle

--- NuxtFullStack/nuxt-travel-log/app.vue
+++ NuxtFullStack/nuxt-travel-log/app.vue
@@ -1,10 +1,5 @@
 <template>
-  <div>
-    <h1 class="text-3xl font-bold font-stretch-150%">
-      Hello Nuxt Woowaofoasiufhaioushf!
-    </h1>
-    <button class="btn btn-secondary">
-      Click for map! <Icon name="tabler:location-filled" size="18" style="color: chartreuse;" />
-    </button>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ NuxtFullStack/nuxt-travel-log/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hero bg-base-300 container mx-auto mt-4">
+    <div class="hero-content text-center min-h-96">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">
+          Welcome to the Travel Log
+        </h1>
+        <p class="py-6">
+          Keep track of where you've been!
+        </p>
+        <button class="btn btn-secondary">
+          Sign in with GitHub! <Icon name="tabler:brand-github" size="24" />
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
