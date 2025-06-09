4afc01a82bdcb6fe7ad3a7d3f89b19a18b3ba65e
alexmccorkle <114952775+alexmccorkle@users.noreply.github.com>
2025-06-09 15:03:36 +0200

    better theme toggle with persistent prefs

---
+++ NuxtFullStack/nuxt-travel-log/components/app/theme-toggle.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+const colorMode = useColorMode();
+
+const isDark = computed({
+  get() {
+    return colorMode.value === "night";
+  },
+  set(value) {
+    colorMode.preference = value ? "night" : "fantasy";
+  },
+});
+</script>
+
+<template>
+  <label class="swap swap-rotate mx-4">
+    <input v-model="isDark" type="checkbox">
+    <Icon
+      class="swap-off"
+      name="tabler:sun-high-filled"
+      size="24"
+    />
+    <Icon
+      class="swap-on"
+      name="tabler:moon-stars"
+      size="24"
+    />
+  </label>
+</template>
