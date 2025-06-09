1bea61f5013a0f4e5874a887bf3e85301a7a5153
Alex McCorkle <114952775+alexmccorkle@users.noreply.github.com>
2025-06-09 14:02:06 +0200

    added tailwind + daisyui + iconify (#50)
    * added tailwind + daisyui + iconify
    
    * feat: add pnpm installation step in GitHub Actions for Nuxt linting
    
    * refactor: linted

--- NuxtFullStack/nuxt-travel-log/app.vue
+++ NuxtFullStack/nuxt-travel-log/app.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="text-3xl font-bold font-stretch-150%">
+      Hello Nuxt Woowaofoasiufhaioushf!
+    </h1>
+    <button class="btn btn-secondary">
+      Click for map! <Icon name="tabler:location-filled" size="18" style="color: chartreuse;" />
+    </button>
   </div>
 </template>
