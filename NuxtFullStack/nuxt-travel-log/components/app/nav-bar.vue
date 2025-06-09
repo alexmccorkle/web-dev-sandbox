f8abb76f44602011d3e6a66eff36dd91f4d09037
Alex McCorkle <114952775+alexmccorkle@users.noreply.github.com>
2025-06-09 15:05:13 +0200

    better theme toggle with persistent prefs (#52)

--- NuxtFullStack/nuxt-travel-log/components/app/nav-bar.vue
+++ NuxtFullStack/nuxt-travel-log/components/app/nav-bar.vue
@@ -6,16 +6,12 @@
       </NuxtLink>
     </div>
     <div class="navbar-end">
-      <div class="flex items-center gap-2">
-        <a class="btn btn-secondary">More</a>
-        <label class="toggle text-base-content">
-          <input type="checkbox" value="fantasy" class="theme-controller">
-
-          <svg aria-label="moon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g stroke-linejoin="round" stroke-linecap="round" stroke-width="2" fill="none" stroke="currentColor"><path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z" /></g></svg>
-          <svg aria-label="sun" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g stroke-linejoin="round" stroke-linecap="round" stroke-width="2" fill="none" stroke="currentColor"><circle cx="12" cy="12" r="4" /><path d="M12 2v2" /><path d="M12 20v2" /><path d="m4.93 4.93 1.41 1.41" /><path d="m17.66 17.66 1.41 1.41" /><path d="M2 12h2" /><path d="M20 12h2" /><path d="m6.34 17.66-1.41 1.41" /><path d="m19.07 4.93-1.41 1.41" /></g></svg>
-
-        </label>
-      </div>
+      <AppThemeToggle />
+      <a class="btn btn-secondary">
+        Sign in with GitHub! <Icon
+          name="tabler:brand-github" size="16"
+        />
+      </a>
     </div>
   </div>
 </template>
